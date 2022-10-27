ae83acc86994379251a8d2a2795bc0b8aabe4b35
karimy <karim.alrashdan14@gmail.com>
2022-10-27 11:55:37 -0600

    Delete dinnerTime class from js and html file

--- main.js
+++ main.js
@@ -2,3 +2,45 @@
 When a user selects a dish option (don’t worry about “Entire Meal”) and then clicks the “Let’s Cook!” button, the user sees a random dish from the list of possible dishes for that category
 When the dish name appears, the cookpot icon disappears
 */
+
+// class DinnerType {
+//     constructor(input) {
+//         this.input = inputs
+//         this.icon = true
+//     }
+//     getRandomIndex(inputs) {
+//         var number = Math.floor(Math.random() * array.length)
+//         input[number(array)]
+//         return number
+//     }
+// }
+// var meal = new DinnerType("cheese")//change this.input to value and pass in value in instance
+// function radioButtonSelection() {
+//     if(document.getElementById("radio1").checked) {
+//         this.input = this.getRandomIndex(sides)
+//         console.log("Side is checked", this.input)
+//     } else if (document.getElementById("radio2").checked) {
+//         this.input = this.getRandomIndex(mains)
+//         console.log("Main dish is checked", this.input)
+//     }else if (document.getElementById("radio3").checked) {
+//         this.input = this.getRandomIndex(desserts)
+//         console.log("Dessert is checked", this.input)
+//     }else if (document.getElementById("radio4").checked) {
+//         // this.input = this.getRandomIndex(sides)
+//         var entireMeal = {
+//             meal1: sides[meal.getRandomIndex(sides)],
+//             meal2: mains[meal.getRandomIndex(mains)],
+//             meal3: desserts[meal.getRandomIndex(desserts)],
+//         }
+//         console.log("Entire meal is checked", entireMeal)
+//     }
+// }
+
+/*When a user selects a dish option (don’t worry about “Entire Meal”) and then clicks the “Let’s Cook!” button, the user sees a random dish from the list of possible dishes for that category
+When the dish name appears, the cookpot icon disappears*/
+
+/* have radioButtonSelection instantiate a new class so that whatever is passed in as
+the argument for input assigns this.input to a value. this.input can even start as null
+we can use this function to assign the input to a array[getRandomIndex(array)]*/
+
+/*this file handles data model updating*/
