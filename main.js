947293eb92b03bef379fe77926f797de70fc6ad2
karimy <karim.alrashdan14@gmail.com>
2022-10-27 12:31:45 -0600

    Create query selectors for radio buttons

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-/*
-When a user selects a dish option (don’t worry about “Entire Meal”) and then clicks the “Let’s Cook!” button, the user sees a random dish from the list of possible dishes for that category
-When the dish name appears, the cookpot icon disappears
-*/
+
+// --------Query Selectors---------//
+var sideButton = document.getElementById("radio1");
+var mainButton = document.getElementById("radio2");
+var dessertButton = document.getElementById("radio3");
+var entireMealButton = document.getElementById("radio4");
 
 // class DinnerType {
 //     constructor(input) {
@@ -35,12 +37,9 @@ When the dish name appears, the cookpot icon disappears
 //         console.log("Entire meal is checked", entireMeal)
 //     }
 // }
-
+//  -------------PseudoCode---------------
 /*When a user selects a dish option (don’t worry about “Entire Meal”) and then clicks the “Let’s Cook!” button, the user sees a random dish from the list of possible dishes for that category
 When the dish name appears, the cookpot icon disappears*/
 
-/* have radioButtonSelection instantiate a new class so that whatever is passed in as
-the argument for input assigns this.input to a value. this.input can even start as null
-we can use this function to assign the input to a array[getRandomIndex(array)]*/
 
-/*this file handles data model updating*/
+
